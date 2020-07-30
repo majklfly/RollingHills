6ee6b49c2d06c90c321761ea9841fc6d676cee0d
majklfly <majklfly@gmail.com>
2020-07-30 21:56:57 +0100

    updated login page, no functionality

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,8 @@ export default {
   primary: {
     textColor: "white",
     fontFamily: "MontSerrat",
+    containerColor: "rgba(0, 0, 128, 0.8)",
+    errorMessageColor: "rgba(	139, 0, 0, 0.8)",
+    buttonColor: "rgba(	20, 182, 185, 1)",
   },
 };
--- src/containers/LoginScreen/LoginScreen.js
+++ src/containers/LoginScreen/LoginScreen.js
@@ -1,11 +1,23 @@
 import React, { useState, useContext } from "react";
-import { Container, Form, Input, Item, Button, Label } from "native-base";
-import { StyleSheet, Text, View } from "react-native";
+import { Form, Item, Button, Label, CheckBox } from "native-base";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 import constants from "../../constants";
 
+import Svg, { Path } from "react-native-svg";
+
 import { Background } from "../../components/Background/Background";
 import { AuthContext } from "../../store/AuthProvider";
+
+import { GoogleLogin } from "../../components/GoogleLogin/GoogleLogin";
+import { FacebookLogin } from "../../components/FacebookLogin/FacebookLogin";
+import { GithubLogin } from "../../components/GithubLogin/GithubLogin";
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState(null);
@@ -15,20 +27,38 @@ const LoginScreen = ({ navigation }) => {
   return (
     <View style={styles.mainContainer}>
       <Background />
-      <Container style={styles.container}>
-        <Form>
+
+      <View style={styles.container}>
+        <View style={{ flexDirection: "row" }}>
+          <Svg
+            height={200}
+            width={200}
+            viewBox="0 0 100 100"
+            style={{ position: "relative", top: "-34.2%" }}
+          >
+            <Path
+              d="M0 0 Q0 55 50 70 L0 70 L0 18"
+              fill={constants.primary.containerColor}
+              stroke={constants.primary.containerColor}
+            />
+          </Svg>
+          <View style={styles.errorMessage}>
+            <Text style={styles.buttonText}>Here is the error message</Text>
+          </View>
+        </View>
+        <Form style={styles.form}>
           <Item floatingLabel>
             <Label style={styles.label}>Email</Label>
-            <Input
-              style={styles.input}
+            <TextInput
+              style={{ color: "red" }}
               autoCorrent={false}
               autoCapitalize="none"
               onChangeText={(value) => setEmail(value)}
-            ></Input>
+            ></TextInput>
           </Item>
           <Item floatingLabel>
             <Label style={styles.label}>Password</Label>
-            <Input
+            <TextInput
               style={styles.input}
               autoCorrent={false}
               autoCapitalize="none"
@@ -36,46 +66,87 @@ const LoginScreen = ({ navigation }) => {
               onChangeText={(value) => {
                 setPassword(value);
               }}
-            ></Input>
+            ></TextInput>
           </Item>
+          <View style={styles.checkbox}>
+            <CheckBox />
+            <Text style={styles.checkboxText}>Remember me</Text>
+          </View>
           <Button
             full
             rounded
-            success
-            style={{ marginTop: 40 }}
+            style={{
+              marginTop: 20,
+              backgroundColor: constants.primary.buttonColor,
+            }}
             onPress={() => login(email, password)}
           >
-            <Text style={styles.terms}>Login</Text>
+            <Text style={styles.buttonText}>Login</Text>
           </Button>
-          <Button
-            full
-            rounded
-            primary
-            style={{ marginTop: 40 }}
-            onPress={() => signup(email, password)}
-          >
-            <Text style={styles.buttonText}>Sign in</Text>
-          </Button>
+          <View style={styles.footer}>
+            <GoogleLogin />
+            <FacebookLogin />
+            <GithubLogin />
+          </View>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <Text
+              style={styles.terms}
+              onPress={() => navigation.navigate("Terms")}
+            >
+              Terms
+            </Text>
+            <Text
+              style={styles.terms}
+              onPress={() => navigation.navigate("Terms")}
+            >
+              Forgot Password?
+            </Text>
+          </View>
         </Form>
-        <Text style={styles.terms} onPress={() => navigation.navigate("Terms")}>
-          Terms
-        </Text>
-      </Container>
+        <View style={{ flexDirection: "row" }}>
+          <Svg
+            height={200}
+            width={200}
+            viewBox="0 0 100 100"
+            style={{ position: "relative", bottom: 74.5 }}
+          >
+            <Path
+              d="M0 0 L0 60 Q0 15 40 0 L0 0"
+              fill={constants.primary.containerColor}
+              stroke={constants.primary.containerColor}
+            />
+          </Svg>
+          <TouchableOpacity style={styles.signupButton}>
+            <Text style={styles.buttonText}>
+              Don't have an account? Sing Up here
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    position: "relative",
+  },
   container: {
-    justifyContent: "center",
-    backgroundColor: "rgba(0, 0, 128, 0.8)",
-    height: 500,
-    width: "80%",
-    alignSelf: "center",
-    marginTop: "40%",
-    borderRadius: 20,
-    padding: 30,
+    flex: 1,
+    backgroundColor: constants.primary.containerColor,
+    borderTopRightRadius: 80,
+    borderBottomRightRadius: 80,
     position: "absolute",
+    width: "100%",
+    height: 500,
+    top: "20%",
+  },
+  form: {
+    width: "70%",
+    alignSelf: "center",
+    position: "relative",
+    top: -150,
   },
   label: {
     color: constants.primary.textColor,
@@ -85,16 +156,55 @@ const styles = StyleSheet.create({
   input: {
     color: constants.primary.textColor,
     fontFamily: constants.primary.fontFamily,
+    width: 70,
+  },
+  checkbox: {
+    flexDirection: "row",
+    paddingLeft: 10,
+    paddingTop: 20,
+  },
+  checkboxText: {
+    color: constants.primary.textColor,
+    fontFamily: constants.primary.fontFamily,
+    marginLeft: 20,
   },
   terms: {
     color: constants.primary.textColor,
     alignSelf: "center",
     paddingTop: 20,
+    paddingHorizontal: 20,
     fontFamily: constants.primary.fontFamily,
   },
   buttonText: {
     color: constants.primary.textColor,
     fontFamily: constants.primary.fontFamily,
+  },
+  footer: {
+    width: "100%",
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  signupButton: {
+    width: "80%",
+    height: 50,
+    backgroundColor: constants.primary.containerColor,
+    position: "relative",
+    left: "-75%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 40,
+  },
+  errorMessage: {
+    width: "80%",
+    height: 50,
+    backgroundColor: constants.primary.errorMessageColor,
+    top: "-25%",
+    left: "-75%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 40,
   },
 });
 
