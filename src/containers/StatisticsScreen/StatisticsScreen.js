e07a68a0b4b47cb2e088e3822c2a87d58901b598
majklfly <majklfly@gmail.com>
2020-08-24 18:00:40 +0100

    dayMode statistics page

--- src/containers/StatisticsScreen/StatisticsScreen.js
+++ src/containers/StatisticsScreen/StatisticsScreen.js
@@ -1,5 +1,12 @@
-import React from "react";
-import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  AsyncStorage,
+} from "react-native";
 
 import { Background } from "../../components/Background/Background";
 import constants from "../../constants";
@@ -10,29 +17,72 @@ const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 
 const StatisticsScreen = () => {
+  const [dayMode, setDayModeLocal] = useState(null);
+
+  useEffect(() => {
+    const retrieveDayMode = async () => {
+      const result = await AsyncStorage.getItem("dayMode");
+      const value = result === "true" ? true : false;
+      setDayModeLocal(value);
+    };
+    retrieveDayMode();
+  });
+
   return (
     <View>
       <Background />
       <View style={styles.mainContainer}>
-        <Text style={styles.title}>Your statistics</Text>
+        <Text style={dayMode ? styles.titleLight : styles.title}>
+          Your statistics
+        </Text>
         <ScrollView>
-          <View style={styles.chartContainer}>
+          <View
+            style={dayMode ? styles.chartContainerLight : styles.chartContainer}
+          >
             <View style={styles.contentLine}>
-              <Text style={styles.contentLabel}>Total distance</Text>
-              <Text style={styles.contentData}>some data</Text>
+              <Text
+                style={dayMode ? styles.contentLabelLight : styles.contentLabel}
+              >
+                Total distance
+              </Text>
+              <Text
+                style={dayMode ? styles.contentDataLight : styles.contentData}
+              >
+                some data
+              </Text>
             </View>
             <View style={styles.contentLine}>
-              <Text style={styles.contentLabel}>Total time</Text>
-              <Text style={styles.contentData}>some data</Text>
+              <Text
+                style={dayMode ? styles.contentLabelLight : styles.contentLabel}
+              >
+                Total time
+              </Text>
+              <Text
+                style={dayMode ? styles.contentDataLight : styles.contentData}
+              >
+                some data
+              </Text>
             </View>
             <View style={styles.contentLine}>
-              <Text style={styles.contentLabel}>Average speed</Text>
-              <Text style={styles.contentData}>some data</Text>
+              <Text
+                style={dayMode ? styles.contentLabelLight : styles.contentLabel}
+              >
+                Average speed
+              </Text>
+              <Text
+                style={dayMode ? styles.contentDataLight : styles.contentData}
+              >
+                some data
+              </Text>
             </View>
           </View>
 
-          <View style={styles.chartContainer}>
-            <Text style={styles.chartTitle}>Distance</Text>
+          <View
+            style={dayMode ? styles.chartContainerLight : styles.chartContainer}
+          >
+            <Text style={dayMode ? styles.chartTitleLight : styles.chartTitle}>
+              Distance
+            </Text>
             <BarChart
               data={{
                 labels: ["15/6", "12/6", "14/6", "8/6", "4/6", "2/6"],
@@ -54,12 +104,24 @@ const StatisticsScreen = () => {
               yAxisSuffix="m"
               yAxisInterval={1} // optional, defaults to 1
               chartConfig={{
-                backgroundColor: constants.primary.containerColor,
-                backgroundGradientFrom: constants.primary.containerColor,
-                backgroundGradientTo: constants.primary.containerColor,
+                backgroundColor: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
+                backgroundGradientFrom: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
+                backgroundGradientTo: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
                 decimalPlaces: 0, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                color: (opacity = 1) =>
+                  dayMode
+                    ? constants.secondary.textColor
+                    : constants.primary.textColor,
+                labelColor: (opacity = 1) =>
+                  dayMode
+                    ? constants.secondary.textColor
+                    : constants.primary.textColor,
                 style: {
                   borderRadius: 16,
                 },
@@ -78,8 +140,12 @@ const StatisticsScreen = () => {
               }}
             />
           </View>
-          <View style={styles.chartContainer}>
-            <Text style={styles.chartTitle}>Average Speed</Text>
+          <View
+            style={dayMode ? styles.chartContainerLight : styles.chartContainer}
+          >
+            <Text style={dayMode ? styles.chartTitleLight : styles.chartTitle}>
+              Average Speed
+            </Text>
             <BarChart
               data={{
                 labels: ["15/6", "12/6", "14/6", "8/6", "4/6", "2/6"],
@@ -101,12 +167,24 @@ const StatisticsScreen = () => {
               yAxisSuffix="km/h"
               yAxisInterval={1} // optional, defaults to 1
               chartConfig={{
-                backgroundColor: constants.primary.containerColor,
-                backgroundGradientFrom: constants.primary.containerColor,
-                backgroundGradientTo: constants.primary.containerColor,
+                backgroundColor: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
+                backgroundGradientFrom: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
+                backgroundGradientTo: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
                 decimalPlaces: 0, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                color: (opacity = 1) =>
+                  dayMode
+                    ? constants.secondary.textColor
+                    : constants.primary.textColor,
+                labelColor: (opacity = 1) =>
+                  dayMode
+                    ? constants.secondary.textColor
+                    : constants.primary.textColor,
                 style: {
                   borderRadius: 16,
                 },
@@ -125,8 +203,12 @@ const StatisticsScreen = () => {
               }}
             />
           </View>
-          <View style={styles.chartContainer}>
-            <Text style={styles.chartTitle}>Time</Text>
+          <View
+            style={dayMode ? styles.chartContainerLight : styles.chartContainer}
+          >
+            <Text style={dayMode ? styles.chartTitleLight : styles.chartTitle}>
+              Time
+            </Text>
             <BarChart
               data={{
                 labels: ["15/6", "12/6", "14/6", "8/6", "4/6", "2/6"],
@@ -148,12 +230,24 @@ const StatisticsScreen = () => {
               yAxisSuffix="s"
               yAxisInterval={1} // optional, defaults to 1
               chartConfig={{
-                backgroundColor: constants.primary.containerColor,
-                backgroundGradientFrom: constants.primary.containerColor,
-                backgroundGradientTo: constants.primary.containerColor,
+                backgroundColor: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
+                backgroundGradientFrom: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
+                backgroundGradientTo: dayMode
+                  ? constants.secondary.containerColor
+                  : constants.primary.containerColor,
                 decimalPlaces: 0, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                color: (opacity = 1) =>
+                  dayMode
+                    ? constants.secondary.textColor
+                    : constants.primary.textColor,
+                labelColor: (opacity = 1) =>
+                  dayMode
+                    ? constants.secondary.textColor
+                    : constants.primary.textColor,
                 style: {
                   borderRadius: 16,
                 },
@@ -194,9 +288,24 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     marginVertical: 10,
   },
+  chartContainerLight: {
+    width: "90%",
+    backgroundColor: constants.secondary.containerColor,
+    height: 300,
+    alignSelf: "center",
+    borderRadius: 15,
+    marginVertical: 10,
+  },
   title: {
     color: constants.primary.textColor,
     fontFamily: constants.primary.fontFamily,
+    fontSize: 35,
+    alignSelf: "center",
+    marginBottom: "5%",
+  },
+  titleLight: {
+    color: constants.secondary.textColor,
+    fontFamily: constants.secondary.fontFamily,
     fontSize: 35,
     alignSelf: "center",
     marginBottom: "5%",
@@ -211,14 +320,30 @@ const styles = StyleSheet.create({
     color: constants.primary.textColor,
     fontFamily: constants.primary.fontFamily,
   },
+  contentLabelLight: {
+    color: constants.secondary.textColor,
+    fontFamily: constants.secondary.fontFamily,
+  },
   contentData: {
     color: constants.primary.textColor,
     fontFamily: constants.primary.fontFamily,
     fontSize: 25,
   },
+  contentDataLight: {
+    color: constants.secondary.textColor,
+    fontFamily: constants.secondary.fontFamily,
+    fontSize: 25,
+  },
   chartTitle: {
     color: constants.primary.textColor,
     fontFamily: constants.primary.fontFamily,
+    fontSize: 25,
+    marginLeft: 20,
+    marginTop: 10,
+  },
+  chartTitleLight: {
+    color: constants.secondary.textColor,
+    fontFamily: constants.secondary.fontFamily,
     fontSize: 25,
     marginLeft: 20,
     marginTop: 10,
