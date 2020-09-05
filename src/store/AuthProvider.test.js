98bfef37b1c618e57659e78bfc38e5eb49f36e3b
majklfly <majklfly@gmail.com>
2020-09-05 18:58:43 +0100

    closest I can get

--- src/store/AuthProvider.test.js
+++ src/store/AuthProvider.test.js
@@ -2,50 +2,44 @@ import React, { useContext } from "react";
 import { AuthContext, GlobalContext, AuthProvider } from "./AuthProvider";
 import Adapter from "enzyme-adapter-react-16";
 import { mount, configure } from "enzyme";
-import { firebase } from "../../firebase";
 
 configure({ adapter: new Adapter() });
 
-// describe("Login", () => {
-//   it("loads the user data to the global", async () => {
-//     const TestComponent = () => {
-//       const { login } = useContext(AuthContext);
-//       const {
-//         state: { user },
-//       } = useContext(GlobalContext);
-//       return (
-//         <>
-//           <div data-testid="user">{user.favorite_food}</div>
-//           <button onClick={login} title="button" data-testid="login"></button>
-//         </>
-//       );
-//     };
+jest.mock("firebase/app", () => ({
+  __esModule: true,
+  default: {
+    apps: [],
+    initializeApp: () => {},
+    auth: () => {},
+  },
+}));
 
-//     const wrapper = mount(
-//       <AuthProvider>
-//         <TestComponent />
-//       </AuthProvider>
-//     );
+describe("Login", () => {
+  it("loads the user data to the global", async () => {
+    const TestComponent = () => {
+      const { login } = useContext(AuthContext);
+      const {
+        state: { user },
+      } = useContext(GlobalContext);
+      return (
+        <>
+          <div data-testid="user">{user.favorite_food}</div>
+          <button onClick={login} title="button" data-testid="login"></button>
+        </>
+      );
+    };
 
-//     expect(wrapper.find("[data-testid='user']").text()).toEqual("");
+    const wrapper = mount(
+      <AuthProvider>
+        <TestComponent />
+      </AuthProvider>
+    );
 
-//     wrapper.find("[data-testid='login']").simulate("click");
-//     firebase.firestore().autoFlush();
-//     firebase
-//       .firestore()
-//       .analytics()
-//       .setUserProperties({ favorite_food: "apples" });
-//     expect(wrapper.find("[data-testid='user']").text()).toEqual("Peter");
-//   });
-// });
+    expect(wrapper.find("[data-testid='user']").text()).toEqual("");
 
-const db = firebase.initializeApp(config).firestore();
-
-describe("New city", () => {
-  it("should create a new city in firestore", async () => {
-    await db.collection("cities").doc("Seattle").set({ state: "WA" });
-    const city = await db.collection("cities").doc("Seattle").get();
-
-    expect(city.data()["population"]).toEqual("WA");
+    wrapper.find("[data-testid='login']").simulate("click");
+    // here happens the magic and it starts to work....
+    //
+    expect(wrapper.find("[data-testid='user']").text()).toEqual("Peter");
   });
 });
