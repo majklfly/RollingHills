17bfd4c405c919e7ea27a939671e48aa2fd82bfe
majklfly <majklfly@gmail.com>
2020-09-21 17:37:27 +0100

    deleted task manager

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useContext } from "react";
 import * as Location from "expo-location";
 
+import { Alert } from "react-native";
+
 import { Accuracy, watchPositionAsync } from "expo-location";
 
 import { LocationContext } from "../store/LocationProvider";
@@ -19,38 +21,18 @@ export default (shouldTrack, callback) => {
         setErr("Permission to access location was denied");
       }
       let position = await Location.getCurrentPositionAsync({});
-      addLocation(position, false);
-      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 5000,
-        distanceInterval: 10,
-      });
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.Balanced,
-          timeInterval: 5000,
-          distanceInterval: 10,
-        },
-        callback
-      );
-      setSubscriber(sub);
+      if (position) {
+        addLocation(position, false);
+      }
     } catch (e) {
-      setErr(e.message);
+      Alert.alert("Problem with useLocation hook");
     }
   };
 
   useEffect(() => {
     if (shouldTrack) {
       startWatching();
-    } else {
-      subscriber.remove();
-      setSubscriber(null);
     }
-    return () => {
-      if (subscriber) {
-        subscriber.remove();
-      }
-    };
   }, [shouldTrack, callback]);
 
   return [err];
